3ae9380436ad51fa61ccd8846f7f6859c840aa93
Dave Liddament <dave@lampbristol.com>
2023-05-16 19:32:20 +0100

    ADD answers for exercise 01

---
+++ build/Phpstan/Rules/DontCallPrintRule.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaveLiddament\PhpstanTutorial\Phpstan\Rules;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+class DontCallPrintRule implements Rule
+{
+    public function getNodeType(): string
+    {
+        return Node\Expr\Print_::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        return [
+            RuleErrorBuilder::message("Don't call print")->build(),
+        ];
+    }
+}
