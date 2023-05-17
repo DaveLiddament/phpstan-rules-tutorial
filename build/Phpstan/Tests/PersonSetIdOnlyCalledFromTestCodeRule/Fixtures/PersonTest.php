98a3faa80629d4c6b46a8dda38b394e5c63677ab
Dave Liddament <dave@lampbristol.com>
2023-05-17 13:30:50 +0100

    Answers 05

---
+++ build/Phpstan/Tests/PersonSetIdOnlyCalledFromTestCodeRule/Fixtures/PersonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaveLiddament\PhpstanTutorial\Phpstan\Tests\PersonSetIdOnlyCalledFromTestCodeRule\Fixtures;
+
+use DaveLiddament\PhpstanTutorial\Person;
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    public function testSetId(): void
+    {
+        $companyEntity = new Person("bob");
+        $companyEntity->setId(7); // OK
+        $this->assertSame(7, $companyEntity->getId());
+    }
+}
